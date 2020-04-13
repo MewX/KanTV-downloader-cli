aa4e4b620141faafccc885b1085be4c9d5278973
MewX <xiayuanzhong@gmail.com>
2020-04-13 18:10:10 +1000

    Moved the project to gazelle build

---
+++ kantv/api/common.go
@@ -0,0 +1,12 @@
+package api
+
+const interface_version = "104"
+const appid = "kantvea2663ea5d5evje09j"
+
+type Sign struct {
+	appid     string
+	noncestr  string // UUID
+	sbID      string // device ID, this should be settable
+	timestamp string // a timestamp string
+	sign      string // the hash
+}
